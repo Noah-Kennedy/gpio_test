f7dfc846c53581ac97918b9afe61a4bf11e9c173
noah <noah.kennedy.professional@gmail.com>
2018-09-16 13:44:14 -0500

    Fixed errors

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,10 @@ fn main() {
 fn blink_led(led_pin: u64) -> sysfs_gpio::Result<()> {
     let my_led = Pin::new(led_pin); // number depends on chip, etc.
     my_led.with_exported(|| {
-        loop {
-            my_led.set_direction(Direction::Low).expect("Failed to set direction!");
-            my_led.set_value(0).unwrap();
-            sleep(Duration::from_millis(200));
-            my_led.set_value(1).unwrap();
-            sleep(Duration::from_millis(200));
-        }
+        my_led.set_direction(Direction::Low).expect("Failed to set direction!");
+        my_led.set_value(0).unwrap();
+        sleep(Duration::from_millis(200));
+        my_led.set_value(1).unwrap();
+        sleep(Duration::from_millis(200));
     })
 }
